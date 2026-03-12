ba954da249220659aacc44fa8d3cfb2188b7b8a0
Sasha Krigel <sasha.krigel@gmail.com>
2026-03-12 13:08:38 -0400

    updating base model

--- config.py
+++ config.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-BASE_MODEL = "meta-llama/Llama-3.1-8B-Instruct"
+# BASE_MODEL = "meta-llama/Llama-3.1-8B-Instruct"
+BASE_MODEL = "meta-llama/Llama-3.3-70B-Instruct"
+
 # Other options:
-MODEL = "Qwen/Qwen2.5-72B-Instruct"
+# MODEL = "Qwen/Qwen2.5-72B-Instruct"
 # MODEL = "HuggingFaceTB/SmolLM3-3B"
 # MODEL = "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
 
