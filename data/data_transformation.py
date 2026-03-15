39ef3b836f5978f81baf95f237ac5782801f9c7f
Margaret <mqwang@mit.edu>
2026-03-15 16:27:10 -0400

    chromadb in-memory implementation

--- data/data_transformation.py
+++ data/data_transformation.py
@@ -3,7 +3,7 @@ import csv
 
 def get_catalogs():
     catalogs = {}
-    terms = ["s26", "f25", "s25", "i25", "f24", "s24", "f23", "s23", "f22"]
+    terms = ["s26", "f25", "s25", "f24", "s24", "f23", "s23", "f22"]
     for term in terms:
         with open(f"data/{term}.json", "r") as f:
             term_data = json.load(f)
@@ -38,11 +38,45 @@ def combine_wtw(subjects, major):
             if y4:
                 subjects[subject]["taken_by"].append(f"{major} seniors")
     
+def get_terms(terms):
+    if "FA" in terms:
+        if "SP" in terms:
+            return "fall and spring"
+        else:
+            return "fall"
+    if "SP" in terms:
+        return "spring"
+
+def data_cleaning(subjects):
+    delete_fields = ["lectureSections", "labSections", "recitationSections", "designSections", "quarterInfo", "b"]
+    empty_fields = ["taken_by", "sectionKinds", "lectureRawSections", "recitationRawSections", "labRawSections", "designRawSections"]
+    for number, subject in subjects.items():
+        document = f"""{number} {subject["name"]} is a class in the {subject["course"]} department.
+        The class description states {subject["description"]}.
+        It is offered in the {get_terms(subject["terms"])}. It has a rating of {subject["rating"]} and takes an average of {subject["hours"]} a week.
+        """
+        if subject["hass"]:
+            document += f"/n It fulfills the HASS-{subject["hass"][0]} graduation requirement."
+        if subject["gir"]:
+            document += f"/n It fulfills the {subject["gir"]} graduation requirement."
+        if subject["comms"]:
+            document += f"/n It fulfills the {subject["comms"]} graduation requirement."
+        subject["document"] = document
+
+        for field in delete_fields:
+            subject.pop(field, "")
+        if not subject["hass"]:
+            subject["hass"] = "E"
+        for field in empty_fields:
+            if field in subject and not subject[field]:
+                subject[field] = "N/A"
+
 if __name__ == "__main__":
     catalogs = get_catalogs()
     subjects = combine_catalogs(catalogs)
     for major in ["6-1", "6-2", "6-3", "6-4", "6-7", "6-14"]:
         combine_wtw(subjects, major)
-    with open("data/combined_catalog.json", "w") as f:
+    data_cleaning(subjects)
+    with open("data/combined_catalog_doc.json", "w") as f:
         json.dump(subjects, f)
             
--- src/chat.py
+++ src/chat.py
@@ -1,11 +1,22 @@
+import chromadb
+import json
 from huggingface_hub import InferenceClient
+from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
 from config import BASE_MODEL, MY_MODEL, HF_TOKEN
 
+""" PROMPT ENGINEERING ONLY
 with open("data/s25_names.txt") as f:
     SPRING_CLASSES = f.read()
 
 with open("src/system_prompt.txt") as f:
     SYSTEM_PROMPT = f.read() + SPRING_CLASSES
+"""
+
+with open("src/system_prompt_rag.txt") as f:
+    SYSTEM_PROMPT = f.read()
+
+NUM_RAG_CHUNKS = 10
+MAX_TOKENS = 1024
 
 class Chatbot:
     """
@@ -22,7 +33,23 @@ class Chatbot:
         """
         model_id = MY_MODEL if MY_MODEL else BASE_MODEL # define MY_MODEL in config.py if you create a new model in the HuggingFace Hub
         self.client = InferenceClient(model=model_id, token=HF_TOKEN)
-        
+        self.db = self.build_chroma_db()
+        print("chatbot and database initialized")
+
+    def build_chroma_db(self):
+        chroma_client = chromadb.Client()
+        embedding_func = DefaultEmbeddingFunction()
+        catalog_collection = chroma_client.create_collection(name="catalog")
+        with open("data/combined_catalog_doc.json", "r") as f:
+            data = json.load(f)
+
+            ids = [number for number in data.keys()]
+            documents = [subject["document"] for subject in data.values()]
+            embeddings = embedding_func([subject["document"] for subject in data.values()])
+            metadata = [subject for subject in data.values()]
+            catalog_collection.upsert(ids=ids, documents=documents, embeddings=embeddings, metadatas=metadata)
+            return catalog_collection
+
     def format_prompt(self, user_input, history=[]):
         """
         TODO: Implement this method to format the user's input into a proper prompt.
@@ -43,7 +70,15 @@ class Chatbot:
              User: {user_input}
              Assistant:"
         """
-        messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+        rag_results = self.db.query(
+            query_texts = [user_input],
+            n_results = NUM_RAG_CHUNKS
+        )
+        print("query returned", rag_results["ids"])
+        system_content = f"""{SYSTEM_PROMPT}. Here is information about {NUM_RAG_CHUNKS} classes relevant to the user's request: {str(rag_results)}.
+                             These classes can be incorporated into your response as you see fit. Do not include any classes not from this list. 
+                          """
+        messages = [{"role": "system", "content": system_content}]
         for msg in history:
             content = msg["content"]
             if isinstance(content, list):
@@ -73,7 +108,7 @@ class Chatbot:
         - Use self.client to generate responses
         """
         messages = self.format_prompt(user_input, history)
-        response = self.client.chat_completion(messages=messages)
+        response = self.client.chat_completion(messages=messages, max_tokens=MAX_TOKENS)
         if not response.choices[0].message.content:
             return "Sorry! Please try again :("
         return response.choices[0].message.content
